fc3b03d00748dcf244f202ee2da505ff19743664
Luigi 2.0 <100915243+LuigiZwei@users.noreply.github.com>
2025-05-13 10:57:17 +0200

    generated project structure

---
+++ src/main/java/net/easykitchen/easykitchen/EasykitchenApplication.java
@@ -0,0 +1,13 @@
+package net.easykitchen.easykitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasykitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasykitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/easykitchen/easykitchen/EasykitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package net.easykitchen.easykitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasykitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
