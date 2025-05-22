08f79abe8478a98facb5b3575264eca99a7139c3
Luigi 2.0 <100915243+LuigiZwei@users.noreply.github.com>
2025-05-22 11:32:48 +0200

    refactored HTML templates and removed unused files for improved organization

--- src/main/java/net/easykitchen/easykitchen/controllers/GroceryController.java
+++ src/main/java/net/easykitchen/easykitchen/controllers/GroceryController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import net.easykitchen.easykitchen.dtos.GroceryDto;
 import net.easykitchen.easykitchen.entities.Grocery;
 import net.easykitchen.easykitchen.mappers.GroceryMapper;
